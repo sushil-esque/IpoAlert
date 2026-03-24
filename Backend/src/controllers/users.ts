e406fca2cbe9fa45f6e571dacb981d008e62ddf6
SushilPrajapati1 <soucesil1@gmail.com>
2026-03-24 11:43:57 +0545

    minor backend fix

--- Backend/src/controllers/users.ts
+++ Backend/src/controllers/users.ts
@@ -1,19 +1,23 @@
 import { NextFunction, Request, Response } from "express";
 import { User } from "../models/users";
-
+import dotenv from "dotenv";
+import path from "path";
+dotenv.config({ path: path.resolve(__dirname, "../../.env") });
 type QueryType = {
   secret?: string;
 };
 export const getUsers = async (
   req: Request<{}, {}, {}, QueryType>,
   res: Response,
-  next: NextFunction
+  next: NextFunction,
 ) => {
-    if(req.query.secret === "suspicioushill" ){
-        const users = await User.find();
-        return res.status(200).send({message:"suceess", data:users})
-    }
-}
+  if (req.query.secret === process.env.QUERY_SECRET) {
+    const users = await User.find();
+    return res.status(200).send({ message: "suceess", data: users });
+  } else {
+    res.status(401).json({ message: "Unauthorized" });
+  }
+};
 
 // export function getUserById(req:Request,res:Response){
 
@@ -21,7 +25,6 @@ export const getUsers = async (
 // }
 
 // export function createUser(req:Request<{},{}, createUserDto>,res:Response<User>){
-  
 
 //     res.status(201).send({email:"",id:1,username:""})
 // }
